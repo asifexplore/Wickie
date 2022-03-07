4318e97a509b32bfbfcd96558596e986d60614f9
DanialAshidiq <77533235+DanialAshidiq@users.noreply.github.com>
2022-03-07 14:49:23 +0800

    Update Activity Page

--- app/src/main/java/com/example/Wickie/features/claims/ViewClaimsActivity.kt
+++ app/src/main/java/com/example/Wickie/features/claims/ViewClaimsActivity.kt
@@ -3,6 +3,7 @@ package com.example.Wickie.features.claims
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.example.Wickie.BaseActivity
+import com.example.Wickie.R
 import com.example.Wickie.databinding.ActivityClaimsformBinding
 import com.example.Wickie.databinding.ActivityMainBinding
 import com.example.Wickie.databinding.ClaimsDetailsBinding
@@ -23,6 +24,14 @@ class ViewClaimsActivity : AppCompatActivity() {
         binding.textViewAmount.text = "$"+amount.toString()
         binding.textViewType.text = type.toString()
         binding.textViewStatus.text = status.toString()
+
+        if(type.toString() == "transport"){
+            binding.imageItem.setImageResource(R.drawable.transport_icon)
+        } else if(type.toString() == "phone"){
+            binding.imageItem.setImageResource(R.drawable.phone_bill)
+        } else {
+            binding.imageItem.setImageResource(R.drawable.meal_icon)
+        }
 
         setContentView(binding.root)
     }
