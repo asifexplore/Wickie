47c7a916c736206eb48ee5d75fda4d6b0ccda06e
MOHD ASIF <mohamedasif167@gmail.com>
2022-03-07 16:56:25 +0800

    Implemented Update Function. - Able to Update data on Firebase. - No image update completed yet - Implemented Image Download from Firebase

--- app/src/main/java/com/example/Wickie/data/source/ClaimRepository.kt
+++ app/src/main/java/com/example/Wickie/data/source/ClaimRepository.kt
@@ -43,39 +43,33 @@ class ClaimRepository {
 
         return mLiveData
     }
+    //    var title: String?, var reason : String? ,var amount : String? , var status:String?, var type : String?,
+//    var imgUrl : String?, var createdDate : String?, var claimDate : String?
+    fun update(title : String,reason : String,amount : String,type : String, imgUrl: String, createdDate : String,claimDate : String, claimID : String ) : MutableLiveData<RequestClaimCall>
+    {
+        val mLiveData = MutableLiveData<RequestClaimCall>()
+        // In Progress
+        requestCall.status = 1
+        requestCall.message = "Fetching Data"
+        mLiveData.value = requestCall
 
-//    fun update(date : String,type : String,reason : String,amount : String, id: String) : MutableLiveData<RequestClaimCall>
-//    {
-//        val mLiveData = MutableLiveData<RequestClaimCall>()
-//        // In Progress
-//        requestCall.status = 1
-//        requestCall.message = "Fetching Data"
-//        mLiveData.value = requestCall
-//
-//        var database : DatabaseReference = FirebaseDatabase.getInstance("https://wickie-3cfa2-default-rtdb.asia-southeast1.firebasedatabase.app").getReference("claims")
-//        // Add onto Firebase
-//        val claim = Claim()
-//        claim.date = date
-//        claim.type = type
-//        claim.reason = reason
-//        claim.amount = amount
-//
-//
-//
-//
-//        database.child("asif").child(id).setValue(claim).addOnSuccessListener {
-//            requestCall.status = 2
-//            requestCall.message = "Add Success"
-//            mLiveData.postValue(requestCall)
-//        }.addOnFailureListener(){
-//            Log.d("ClaimRepo", "Failed")
-//            requestCall.status = 1
-//            requestCall.message = "Add Failed"
-//            mLiveData.postValue(requestCall)
-//        }
-//
-//        return mLiveData
-//    }
+        var database : DatabaseReference = FirebaseDatabase.getInstance("https://wickie-3cfa2-default-rtdb.asia-southeast1.firebasedatabase.app").getReference("claims")
+        // Add onto Firebase
+        val claim = Claim(title, reason, amount, "Pending" ,type,imgUrl, createdDate,claimDate, claimID)
+
+        database.child("asif").child(claimID).setValue(claim).addOnSuccessListener {
+            requestCall.status = 2
+            requestCall.message = "Add Success"
+            mLiveData.postValue(requestCall)
+        }.addOnFailureListener(){
+            Log.d("ClaimRepo", "Failed")
+            requestCall.status = 1
+            requestCall.message = "Add Failed"
+            mLiveData.postValue(requestCall)
+        }
+
+        return mLiveData
+    }
 /*
     fun retrieve(username:String, password: String) : MutableLiveData<RequestAuthCall>
     {
--- app/src/main/java/com/example/Wickie/features/claims/ClaimViewModel.kt
+++ app/src/main/java/com/example/Wickie/features/claims/ClaimViewModel.kt
@@ -5,10 +5,19 @@ import androidx.lifecycle.ViewModel
 import com.example.Wickie.data.source.ClaimRepository
 import com.example.Wickie.data.source.data.RequestClaimCall
 import com.example.Wickie.Utils.getCurrentDateTime
+import com.example.Wickie.data.source.data.Claim
 
 class ClaimViewModel : ViewModel() {
 
     private val claimRepository: ClaimRepository = ClaimRepository()
+
+    // 0 = Adding
+    // 1 = Updating
+    var pageType = MutableLiveData<Int>()
+
+    // Set claim object when user clicks on claim from recyclerView
+    val chosenClaim : Claim  = Claim("tst4", "stst" ,"123" , "Pending", "Transport",
+    "2022_02_24_08_26_21", "Mon Mar 07 07:13:43 GMT 2022", "Choose Date", "16")
 
     // Claim Form Status
     var descriptionData = arrayOf("Details", "Image", "Done")
@@ -16,6 +25,7 @@ class ClaimViewModel : ViewModel() {
 
     init {
         pageStatus.value = 1
+        pageType.value = 1 // Hardcoded to 0 for testing Update Feature
     }
 
     fun incrementPageStatus()
@@ -43,8 +53,8 @@ class ClaimViewModel : ViewModel() {
         return claimRepository.create(title,reason,amount,type, imgUrl, dateInString,claimDate)
     }
 
-//    fun update(date : String,type : String,reason : String,amount : String, id: String)  : MutableLiveData<RequestClaimCall>
-//    {
-//        return claimRepository.update(date,type,reason,amount, id)
-//    }
+    fun update(title : String,reason : String,amount : String,type : String, imgUrl: String,claimDate : String)  : MutableLiveData<RequestClaimCall>
+    {
+        return claimRepository.update(title,reason,amount,type, imgUrl, chosenClaim.createdDate.toString(),claimDate, chosenClaim.claimID.toString())
+    }
 }
