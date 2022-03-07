e4a0888e347e3e8b438303f63660c08deeec3315
DanialAshidiq <77533235+DanialAshidiq@users.noreply.github.com>
2022-03-07 16:44:27 +0800

    Cleanup View-Claims Activity

--- app/src/main/java/com/example/Wickie/features/claims/ViewClaimsActivity.kt
+++ app/src/main/java/com/example/Wickie/features/claims/ViewClaimsActivity.kt
@@ -2,10 +2,7 @@ package com.example.Wickie.features.claims
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.example.Wickie.BaseActivity
 import com.example.Wickie.R
-import com.example.Wickie.databinding.ActivityClaimsformBinding
-import com.example.Wickie.databinding.ActivityMainBinding
 import com.example.Wickie.databinding.ClaimsDetailsBinding
 
 class ViewClaimsActivity : AppCompatActivity() {
@@ -13,11 +10,11 @@ class ViewClaimsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ClaimsDetailsBinding.inflate(layoutInflater)
-        var title = intent.getStringExtra("title")
-        var claimDate = intent.getStringExtra("claimDate")
-        var amount = intent.getStringExtra("amount")
-        var type = intent.getStringExtra("type")
-        var status = intent.getStringExtra("status")
+        val title = intent.getStringExtra("title")
+        val claimDate = intent.getStringExtra("claimDate")
+        val amount = intent.getStringExtra("amount")
+        val type = intent.getStringExtra("type")
+        val status = intent.getStringExtra("status")
 
         binding.textViewTitle.text = title.toString()
         binding.textViewDate.text = claimDate.toString()
@@ -35,10 +32,5 @@ class ViewClaimsActivity : AppCompatActivity() {
 
         setContentView(binding.root)
     }
-
-
-
-
-
 
 }
