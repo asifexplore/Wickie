c023f793cb85234cca8391d6ea09805b3903d030
DanialAshidiq <77533235+DanialAshidiq@users.noreply.github.com>
2022-04-06 11:24:06 +0800

    Update ChatBot classes with Comments

--- app/src/main/java/com/example/Wickie/features/chatbot/ChatBotViewModel.kt
+++ app/src/main/java/com/example/Wickie/features/chatbot/ChatBotViewModel.kt
@@ -2,7 +2,19 @@ package com.example.Wickie.features.chatbot
 
 import androidx.lifecycle.ViewModel
 import com.example.Wickie.data.source.ChatbotRepository
-
+/*
+*  ChatBotViewModel contains the function to connect to heroku webserver,
+*
+* Functions Within:
+* ==========================================================================
+* Function Name: connect
+* Function Purpose: connect to heroku server
+* Function Arguments: takes in the APIService as an argument
+* Results:
+*         Success: Connection established to the server
+*         Failed:
+*---------------------------------------------------
+*/
 class ChatBotViewModel( )  : ViewModel() {
 
     private val ChatbotRepository : ChatbotRepository = ChatbotRepository()
